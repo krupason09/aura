b49ecfb6fc9aa8964a1a09ba7b4737d67c0e8987
KRUPASON <krupason664@gmail.com>
2026-04-30 16:23:02 +0530

    Update profile.py

--- src/pygpt_net/ui/dialog/profile.py
+++ src/pygpt_net/ui/dialog/profile.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.04.09 23:00:00                  #
-# ================================================== #
-
 from PySide6.QtGui import QStandardItemModel
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout
 
