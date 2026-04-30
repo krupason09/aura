9ed3b9b9cbc09d35127798a8158ec2498b7ac2aa
KRUPASON <krupason664@gmail.com>
2026-04-30 16:30:26 +0530

    Update applog.py

--- src/pygpt_net/ui/dialog/applog.py
+++ src/pygpt_net/ui/dialog/applog.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.03.25 12:00:00                  #
-# ================================================== #
-
 import os
 
 from PySide6.QtGui import QTextCursor, Qt
