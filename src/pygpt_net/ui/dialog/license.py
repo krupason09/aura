430e6b79fd8d1f435190a1879b67c98782d96ca4
KRUPASON <krupason664@gmail.com>
2026-04-30 16:26:30 +0530

    Update license.py

--- src/pygpt_net/ui/dialog/license.py
+++ src/pygpt_net/ui/dialog/license.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.02.01 18:00:00                  #
-# ================================================== #
-
 import os
 
 from PySide6.QtCore import Qt
