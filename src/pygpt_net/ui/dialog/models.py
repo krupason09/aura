73f68b9ebada28f0aca1131219a0e7d05352f5eb
KRUPASON <krupason664@gmail.com>
2026-04-30 16:25:38 +0530

    Update models.py

--- src/pygpt_net/ui/dialog/models.py
+++ src/pygpt_net/ui/dialog/models.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2026.01.22 17:00:00                  #
-# ================================================== #
-
 import copy
 from typing import Dict, List, Optional
 
