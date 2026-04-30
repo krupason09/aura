f7278ec64873e3ac5093dd3034ec7624edebf6cb
KRUPASON <krupason664@gmail.com>
2026-04-30 16:29:22 +0530

    Update create.py

--- src/pygpt_net/ui/dialog/create.py
+++ src/pygpt_net/ui/dialog/create.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.01.27 19:00:00                  #
-# ================================================== #
-
 from pygpt_net.ui.widget.dialog.create import CreateDialog
 from pygpt_net.utils import trans
 
