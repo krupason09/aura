a3126669d3c723940854c32180a16f35d4b75f16
KRUPASON <krupason664@gmail.com>
2026-04-30 16:25:16 +0530

    Update models_importer.py

--- src/pygpt_net/ui/dialog/models_importer.py
+++ src/pygpt_net/ui/dialog/models_importer.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2025.08.24 23:00:00                  #
-# ================================================== #
 from PySide6 import QtCore
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout, QLabel
