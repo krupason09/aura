1f32ce078666060950cbefb1939c0ae8ff793c00
KRUPASON <krupason664@gmail.com>
2026-04-30 16:31:36 +0530

    Update __init__.py

--- src/pygpt_net/ui/dialog/__init__.py
+++ src/pygpt_net/ui/dialog/__init__.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.05 22:00:00                  #
-# ================================================== #
+
