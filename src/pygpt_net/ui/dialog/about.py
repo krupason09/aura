6597f36def5e0ed555aae9463883f7cb9dbc860e
KRUPASON <krupason664@gmail.com>
2026-04-30 16:30:47 +0530

    Update about.py

--- src/pygpt_net/ui/dialog/about.py
+++ src/pygpt_net/ui/dialog/about.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2026.01.21 02:00:00                  #
-# ================================================== #
-
 import os
 
 from PySide6.QtGui import QPixmap, Qt, QIcon
