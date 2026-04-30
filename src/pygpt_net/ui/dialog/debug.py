176878c1b9d98ee1c43dbee40d5321d6dacb6957
KRUPASON <krupason664@gmail.com>
2026-04-30 16:28:34 +0530

    Remove header comments from debug.py
    Removed header comments and license information from debug.py.

--- src/pygpt_net/ui/dialog/debug.py
+++ src/pygpt_net/ui/dialog/debug.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2026.01.21 01:00:00                  #
-# ================================================== #
-
 from PySide6.QtCore import Qt, QEvent, QTimer, QObject
 from PySide6.QtWidgets import QScrollArea, QSplitter, QSplitterHandle, QPushButton, \
     QCheckBox, QHBoxLayout, QAbstractItemView, QLabel, QVBoxLayout
