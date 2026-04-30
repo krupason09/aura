bbbc4f73271138a7411857998bca29957d500fe1
KRUPASON <krupason664@gmail.com>
2026-04-30 16:26:04 +0530

    Remove header comments from logger.py
    Removed header comments from logger.py file.

--- src/pygpt_net/ui/dialog/logger.py
+++ src/pygpt_net/ui/dialog/logger.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
-# MIT License                                        #
-# Created By  : Marcin Szczygliński                  #
-# Updated Date: 2026.01.20 21:00:00                  #
-# ================================================== #
-
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout
 
 from pygpt_net.ui.widget.dialog.logger import LoggerDialog
